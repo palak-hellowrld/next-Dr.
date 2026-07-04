e552baf585028587c727481485156237e126609b
Palak Mandloi <palakmandloi7@gmail.com>
2026-07-04 00:41:35 -0700

    Add / route with GET/POST clue-guessing logic, Flask session tracking for clue progress, and fix DetachedInstanceError by keeping Term access inside DB session

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
 
-from flask import Flask
+from flask import Flask, session as flaskSession, request
 import model
+from sqlalchemy.orm import Session
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def index():
-    specialty_list = []
-    categories = model.session.query(model.Category).all()
-    for category in categories:
-        specialty_list.append(category.specialty)
-    categories_string = ", ".join(specialty_list)
-    return categories_string
+    with Session(model.engine) as session:
+        todaysTerm =  model.getTodaysTerm(session)
+    
+        clueNumber=flaskSession.get("clueNumber", 1)
+
+        if request.method == "POST":
+            userGuess=request.form.get("guess")
+            if userGuess==todaysTerm.term_name:
+                return (f"Awesome! Today's word was {todaysTerm.term_name}")
+            else:
+                if clueNumber<4:
+                    clueNumber+=1
+                    flaskSession["clueNumber"]=clueNumber
+                    return f"Clue {clueNumber}: , {getattr(todaysTerm, f'clue{clueNumber}')}"
+                else:
+                    return(f"Almost! Today's word was {todaysTerm.term_name}. Better luck tomorrow!")
+        
+        return (f"Clue {clueNumber}: {getattr(todaysTerm, f'clue{clueNumber}')}")
+            
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000)
